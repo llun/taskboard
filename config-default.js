768d53a5fec01de3bea2d83825f06b27fe3e637b
Maythee Anegboonlap <null@llun.in.th>
2011-09-14 11:17:44 +0700

    server.js for serve static file with node.

---
+++ config-default.js
@@ -0,0 +1,9 @@
+var config = {
+  // Default server port
+  port: 8080,
+  
+  // Base static path
+  base: '/static'
+}
+
+exports.config = config;
---
+++ server.js
@@ -0,0 +1,50 @@
+var http = require('http'),
+    nowjs = require('now'),
+    path = require('path'),
+    fs = require('fs'),
+    mime = require('mime');
+
+// Parse configuration
+var config = null;
+if (path.existsSync('config.js')) {
+  config = require('./config.js').config;
+} else {
+  config = require('./config-default.js').config;
+}
+
+// Initial server and now.js
+var httpServer = http.createServer(
+  function(request, response) {
+    
+    var url = request.url == '/' ? '/index.html' : request.url;
+    var filePath = path.join(__dirname, config.base, url);
+    
+    if (path.existsSync(filePath)) {
+      // Serve static file
+      var stat = fs.statSync(filePath);
+      
+      response.writeHead(200, {
+        'Content-Type': mime.lookup(filePath),
+        'Content-Length': stat.size
+      });
+      
+      var stream = fs.createReadStream(filePath);
+      stream.on('data', function (data) {
+        response.write(data);
+      });
+      
+      stream.on('end', function() {
+        response.end();
+      });
+      
+    } else {
+      // Response 404
+      response.writeHead(404, {})
+      response.end("Not found")
+    }
+    
+  });
+httpServer.listen(config.port);
+
+var everyone = nowjs.initialize(httpServer);
+  
