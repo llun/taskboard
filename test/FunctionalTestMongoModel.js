e4c5ac1b57fce4b11e64458f3ac8a207c9b9d6b2
Maythee Anegboonlap <null@llun.in.th>
2011-11-29 23:39:22 +0700

    Add list methods.

--- test/FunctionalTestMongoModel.js
+++ test/FunctionalTestMongoModel.js
@@ -24,13 +24,12 @@ testit('TestMongoModel', {
         local.client = client;
         local.collections = new mongodb.Collection(client, 'item_collection');
         
-        var fixtures = [{
-          name: 'Project1',
-          children: [1, 2, 3]
-        }, {
-          name: 'Project2',
-          children: [3, 4]
-        }];
+        var fixtures = [];
+        for (var index = 1; index <= 30; index++) {
+          fixtures.push({
+            name: 'Project ' + index
+          })
+        }
         
         local.collections.insertAll(fixtures, this);
       },
@@ -78,7 +77,7 @@ testit('TestMongoModel', {
         
         model.create({
           id: uuid.v4(),
-          name: 'Project 3',
+          name: 'Project 31',
           children: []
         }, this);
       },
@@ -103,7 +102,7 @@ testit('TestMongoModel', {
       function () {
         require['create with uuid'] = true;
         
-        test.assert(3, output.count);
+        test.assert(31, output.count);
         test.assertEqual(output.target.id, output.target._id);
       });
     
@@ -128,7 +127,7 @@ testit('TestMongoModel', {
         model = local.model;
         
         model.create({
-          name: 'Project 4',
+          name: 'Project 32',
           children: []
         }, this);
       },
@@ -153,11 +152,130 @@ testit('TestMongoModel', {
       function () {
         require['create without uuid'] = true;
         
-        test.assert(4, output.count);
+        test.assert(32, output.count);
       });
   },
   
-  'test list': function (test) {
+  'test list from 0 limit 2': function (test) {
+    var model;
     
+    var done = false;
+    var output = {};
+    
+    step(
+      function begin () {
+        test.waitFor(
+          function () {
+            return require['create without uuid'];
+          }, this)
+      },
+      function logic () {
+        model = local.model;
+        model.list(0, 2, this);
+      },
+      function found (error, docs) {
+        if (!error) {
+          output.count = docs.length;
+          output.items = docs;
+        } else {
+          logger.error (error);
+        }
+        
+        done = true;
+      });
+      
+    test.waitFor(
+      function (time) {
+        return done;
+      },
+      function () {
+        require['list from 0 with limit 2'] = true;
+        
+        test.assert(2, output.count);
+        test.assertEqual('Project 1', output.items[0].name);
+        test.assertEqual('Project 2', output.items[1].name);
+      });
+  },
+  'test list without offset and limit': function (test) {
+    var model;
+    
+    var done = false;
+    var output = {};
+    
+    step(
+      function begin () {
+        test.waitFor(
+          function () {
+            return require['list from 0 with limit 2'];
+          }, this)
+      },
+      function logic () {
+        model = local.model;
+        model.list(null, null, this);
+      },
+      function found (error, docs) {
+        if (!error) {
+          output.count = docs.length;
+          output.items = docs;
+        } else {
+          logger.error (error);
+        }
+        
+        done = true;
+      });
+      
+    test.waitFor(
+      function (time) {
+        return done;
+      },
+      function () {
+        require['list without limit'] = true;
+        
+        test.assert(25, output.count);
+      });
+  },
+  
+  'test list with offset 5 and limit 9': function (test) {
+    var model;
+    
+    var done = false;
+    var output = {};
+    
+    step(
+      function begin () {
+        test.waitFor(
+          function () {
+            return require['list without limit'];
+          }, this)
+      },
+      function logic () {
+        model = local.model;
+        model.list(5, 9, this);
+      },
+      function found (error, docs) {
+        if (!error) {
+          output.count = docs.length;
+          output.items = docs;
+        } else {
+          logger.error (error);
+        }
+        
+        done = true;
+      });
+      
+    test.waitFor(
+      function (time) {
+        return done;
+      },
+      function () {
+        require['list with offset 5 and limit 9'] = true;
+        
+        test.assert(9, output.count);
+        for (var index in output.items) {
+          var item = output.items[index];
+          var position = Number(index) + 6;
+          test.assertEqual('Project ' + position, item.name);
+        }
+      });
   }
 });
